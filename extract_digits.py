66f6b73248a207f908d61bed23be930326e89af9
Parv Gupta <140427136+ParvGupta7@users.noreply.github.com>
2026-04-05 23:38:40 +0530

    Add files via upload

---
+++ extract_digits.py
@@ -0,0 +1,14 @@
+# Extract digits from a number
+# Eg, input = 5678, so extract each digit from back, and print each number individually.
+
+
+n = int(input("Enter the number: "))
+
+while (n!=0):
+    print(n%10)
+    n=n//10
+
+
+
+
+
