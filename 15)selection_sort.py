f441070b22a95789b95cb96d97265cbca9c940f4
Parv Gupta <140427136+ParvGupta7@users.noreply.github.com>
2026-04-18 23:12:53 +0530

    Add files via upload

---
+++ 15)selection_sort.py
@@ -0,0 +1,14 @@
+# Algorithm to sort array in ascending order.
+
+
+nums = [5,7,8,4,1,6,9,2]
+
+# How it works:
+'''
+We take 5, and traverse from 7 to 2, and see and find the smallest element.
+Then we simply replace it with 5. 
+# Note: Loop must run till the end, otherwise it may stop at 4 only (cuz its smaller than 5).
+
+
+'''
+
