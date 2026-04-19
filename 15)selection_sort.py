f2cb392f69850b0d2f66cdd80ee27627ab6798af
Parv Gupta <140427136+ParvGupta7@users.noreply.github.com>
2026-04-19 13:17:17 +0530

    Add selection sort implementation in Python
    Implement selection sort algorithm with comments on time and space complexity.

--- 15)selection_sort.py
+++ 15)selection_sort.py
@@ -9,6 +9,21 @@ We take 5, and traverse from 7 to 2, and see and find the smallest element.
 Then we simply replace it with 5. 
 # Note: Loop must run till the end, otherwise it may stop at 4 only (cuz its smaller than 5).
 
-
+Try doing this code on my own, just from the logic, then it will be easy to understand the entire thing, and why min_index is needed etc.
 '''
 
+
+for i in range(0,len(nums)):
+    min_index = i
+    for j in range(i+1,len(nums)):
+        if nums[j] < nums[min_index]:
+            min_index = j
+    
+    nums[i],nums[min_index] = nums[min_index],nums[i]
+
+
+# Time complexity: O(n^2)
+# Space complexity: O(1), fixed number of variables, i, j, min_index, nums... they all just update themselves rather than creating new ones.
+
+
+# Also, can try doing selection sort, but to get the array in descending order.
