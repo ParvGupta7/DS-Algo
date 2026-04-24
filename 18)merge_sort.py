0d8bc221c06d1217326e177fee49d88b3af15f25
Parv Gupta <140427136+ParvGupta7@users.noreply.github.com>
2026-04-24 22:41:16 +0530

    Implement merge function
    Added a function to merge two sorted arrays and updated comments for clarity.

--- 18)merge_sort.py
+++ 18)merge_sort.py
@@ -3,7 +3,7 @@
 # It works on Divide & Conquer technique, or we can say Divide and Merge.
 
 # It has 2 components:
-# Component 1: It keeps on dividing an array into halves, until individual/atomic elements are left.
+# Component 1: It keeps on dividing an array into halves, until individual/atomic elements are left. These atomic elements are called "sorted". And then we keep on merging those sorted arrays.
 # Component 2: Then it merges those elements back in sorted order.
 
 nums = [3,1,2,4,1,5,2,6,4]
@@ -68,7 +68,67 @@ print(arr3)
 '''
 
 
+# Create a function that merges two arrays named left and right
+
+
+
+def merge_sorted_arrays(left,right):
+    i = 0 # left
+    j = 0 # right
+    combined = []
+
+    while(i < len(left) and j < len(right)):
+        if left[i]<right[j]:
+            combined.append(left[i])
+            i+=1
+        elif right[j]<left[i]:
+            combined.append(right[j])
+            j+=1
+        else: # both equal
+            combined.append(left[i])
+            combined.append(right[j])
+            i+=1
+            j+=1
+    while(i < len(left)):
+        combined.append(left[i])
+        i+=1
+    while(j < len(right)):
+        combined.append(right[j])
+        j+=1
+    
+    return combined
+
+
+
+left= [4]
+right = [3]
+
+print(merge_sorted_arrays(left,right))
+
+
+'''
+Now, i get the idea.... its like once the whole array is divided into atomic elements, we consider them sorted, then we keep on combining them.
+So we can say if len(arr)<=1, then it is already sorted. It could be empty too [].
+'''
+
+
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr  # Base case
+    mid = len(arr//2) # floor
+
+    left_arr = nums[:mid] # In slicing, when we dont specify the start parameter, python defaults to 0, so we can also write this as nums[0:mid]
+
+    right_arr = nums[mid:] # Similarly here we can also write it as nums[mid:len(nums)]
+
+    left = merge_sort(left_arr)
+    right = merge_sort(right_arr)
+
+    return merge_sorted_arrays(left,right)
 
 
 
 
+
+
+    
